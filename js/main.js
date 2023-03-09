af6483ebe60dabba089cc2fba7ee551fa95c0dfd
VeeOliver <98466281+VeeOliver@users.noreply.github.com>
2023-03-10 00:21:13 +0100

    basic data fetch from JSON file, addition of the navbar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import '../style.css'
+
 async function getJSON(url) {
   let rawData = await fetch(url)
   let data = await rawData.json()
@@ -5,8 +7,24 @@ async function getJSON(url) {
 }
 
 async function start() {
-  let result = await getJSON('./json/books.json');
-  console.log(result)
+  let books = await getJSON('./json/books.json')
+  let html = '';
+  for (let book of books) {
+    html += `
+      <div class="bookThumbnail">
+        <img src="/imgs/bookPlaceholderImage.jpg" class="thumbnail">
+          <h3>${book.title}</h3>
+        <p>Author:${book.author}</p>
+     <p>Price:${book.price}</p>
+    `;
+    /*  for (let key in book) {
+       let value = book[key];
+       html += `<p><span>${key}:</span> ${value}</p>`
+     }
+     html += '</div>'; // end the person div */
+  }
+  document.querySelector('main').innerHTML = html;
 }
+
 
 start();
